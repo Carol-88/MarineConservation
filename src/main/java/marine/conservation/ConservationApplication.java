cb80a319090c9053937a23260c6ffcd20e0a4240
Carol-88 <gralleta@gmail.com>
2025-08-29 08:58:23 +0200

    First commit

---
+++ src/main/java/marine/conservation/ConservationApplication.java
@@ -0,0 +1,13 @@
+package marine.conservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConservationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/marine/conservation/ConservationApplicationTests.java
@@ -0,0 +1,13 @@
+package marine.conservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
