88155b632571550b2774619f36bb82ab1374aaad
Ramix76 <framos-p@student.42barcelona.com>
2025-09-01 12:18:16 +0200

    test for eventcontroller

--- src/main/java/marine/conservation/model/Location.java
+++ src/main/java/marine/conservation/model/Location.java
@@ -2,6 +2,7 @@ package marine.conservation.model;
 
 import jakarta.persistence.Embeddable;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -9,6 +10,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Embeddable
+@Builder
 public class Location {
 
     private String type;      // Ejemplo: BEACH, HARBOR, OCEAN
@@ -16,4 +18,3 @@ public class Location {
     private Double latitude;  // Coordenada
     private Double longitude; // Coordenada
 }
-
